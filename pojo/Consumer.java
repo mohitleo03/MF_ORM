2b4c8e6ab6d88f4c856419e80cd7c9ffd15c5aa8
mohitleo03 <79054705+mohitleo03@users.noreply.github.com>
2021-03-29 18:26:20 +0530

    Add files via upload

---
+++ pojo/Consumer.java
@@ -0,0 +1,11 @@
+package pojo;
+
+public class Consumer {
+	Producer object;
+	public Consumer(Producer object) {
+		this.object = object;
+	}
+	public void work() {
+		object.first();
+	}
+}
---
+++ pojo/Producer.java
@@ -0,0 +1,5 @@
+package pojo;
+
+public interface Producer {
+	public void first();
+}
